068aaf552fba47bd3f0370678112b8009e0f932f
Kully <adam@plot.ly>
2020-05-19 15:21:46 -0400

    fix(cursors): shifted so their placement makes sense in Safari and Chrome

--- js/tools.js
+++ js/tools.js
@@ -2,22 +2,22 @@ const Tools = {
     "pencil": {
         "button-id": "pencil-button",
         "hotkey": "KeyN",
-        "cursor": 'url("img/pencil.png") -16 28, auto',
+        "cursor": 'url("img/pencil.png") 2 28, auto',
     },
     "fill": {
         "button-id": "fill-button",
         "hotkey": "KeyB",
-        "cursor": 'url("img/fill.png") 28 16, auto',
+        "cursor": 'url("img/fill.png") 28 22, auto',
     },
     "eraser": {
         "button-id": "eraser-button",
         "hotkey": "KeyE",
-        "cursor": 'url("img/eraser.png") 0 34, auto',
+        "cursor": 'url("img/eraser.png") 1 16, auto',
     },
     "colorpicker": {
         "button-id": "colorpicker-button",
         "hotkey": "KeyV",
-        "cursor": 'url("img/colorpicker.png") -32 32, auto',
+        "cursor": 'url("img/colorpicker.png") 3 30, auto',
     },
     "selection": {
         "button-id": "selection-button",
