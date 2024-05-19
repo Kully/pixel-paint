3cec98318c68f8a66f74b2936887961025fccc9a
Viet281101 <nva281101@gmail.com>
2024-05-19 11:26:44 +0200

    revert tab to space

--- js/script.js
+++ js/script.js
@@ -10,296 +10,296 @@ const BUTTON_DOWN_OUTLINE = "1px solid blue";
 const CANVAS_INIT_COLOR = palette_color_array[0];
 let ACTIVE_COLOR_SELECT = "firstColor";
 const STATE = {
-	"firstColor": palette_color_array[palette_color_array.length - 1],
-	"secondColor": palette_color_array[0],
-	"activeTool": "pencil-button",
-	"brushDown": false,
-	"grid": {
-		"KeyG_Counter": 0,
-		"hotkey": "KeyG",
-		"isToggled": false,
-	},
-	"altKeyDown": false,
-	"selection": {
-		"totalCount": 0,
-		"startX": 0,
-		"startY": 0,
-		"isLocked": false,
-		"floatingCopy": false,
-	},
-	"selectionCopy": {
-		"initCursorX": 0,
-		"initCursorY": 0,
-		"left": 0,
-		"top": 0,
-		"colorArray": [],
-	},
+    "firstColor": palette_color_array[palette_color_array.length - 1],
+    "secondColor": palette_color_array[0],
+    "activeTool": "pencil-button",
+    "brushDown": false,
+    "grid": {
+        "KeyG_Counter": 0,
+        "hotkey": "KeyG",
+        "isToggled": false,
+    },
+    "altKeyDown": false,
+    "selection": {
+        "totalCount": 0,
+        "startX": 0,
+        "startY": 0,
+        "isLocked": false,
+        "floatingCopy": false,
+    },
+    "selectionCopy": {
+        "initCursorX": 0,
+        "initCursorY": 0,
+        "left": 0,
+        "top": 0,
+        "colorArray": [],
+    },
 }
 const PALETTE_DISPLAY = {
-	nes: document.getElementById("palette-div"),
-	gameboy: document.getElementById("gameboy-palette-div"),
-	radioNES: document.getElementById("radioNES"),
-	radioGameboy: document.getElementById("radioGB")
+    nes: document.getElementById("palette-div"),
+    gameboy: document.getElementById("gameboy-palette-div"),
+    radioNES: document.getElementById("radioNES"),
+    radioGameboy: document.getElementById("radioGB")
 }
 
 
 function Canvas_Cursor_XY(e)
 {
-	let parentCell = e.target.closest("div.canvasCell");
-	let x = parentCell.offsetLeft;
-	let y = parentCell.offsetTop;
-	return [x, y];
+    let parentCell = e.target.closest("div.canvasCell");
+    let x = parentCell.offsetLeft;
+    let y = parentCell.offsetTop;
+    return [x, y];
 }
-	
+    
 function Canvas_Cursor_XY_Rounded_To_Neareset_Cell_Corner(e)
 {
-	let parentCell = e.target.closest("div.canvasCell");
-	let parentCellId = parseInt(parentCell.id);
-	const maxId = Math.pow(CELLS_PER_ROW, 2) - 1;
+    let parentCell = e.target.closest("div.canvasCell");
+    let parentCellId = parseInt(parentCell.id);
+    const maxId = Math.pow(CELLS_PER_ROW, 2) - 1;
 
-	let x = 0;
-	if( e.offsetX <= Math.floor( CELL_WIDTH_PX / 2 ) )
-	{
-		x = parentCell.offsetLeft;
-	}
-	else if( Get_X_From_CellInt(Number(parentCellId)) === CELLS_PER_ROW - 1 )
-	{
-		x = parentCell.offsetLeft + CELL_WIDTH_PX;  // right side of canvas
-	}
-	else
-	{
-		let rightCell = document.getElementById(Pad_Start_Int(parentCellId+1));
-		x = rightCell.offsetLeft;
-	}
+    let x = 0;
+    if( e.offsetX <= Math.floor( CELL_WIDTH_PX / 2 ) )
+    {
+        x = parentCell.offsetLeft;
+    }
+    else if( Get_X_From_CellInt(Number(parentCellId)) === CELLS_PER_ROW - 1 )
+    {
+        x = parentCell.offsetLeft + CELL_WIDTH_PX;  // right side of canvas
+    }
+    else
+    {
+        let rightCell = document.getElementById(Pad_Start_Int(parentCellId+1));
+        x = rightCell.offsetLeft;
+    }
 
-	let y = 0;
-	if( e.offsetY <= Math.floor( CELL_WIDTH_PX / 2 ) )
-	{
-		y = parentCell.offsetTop;
-	}
-	else
-	if( parseInt(parentCell.id)+CELLS_PER_ROW > maxId )  // bottom of canvas
-	{
-		y = parentCell.offsetTop + CELL_WIDTH_PX;
-	}
-	else
-	{
-		let cellIdBelow = Pad_Start_Int(parentCellId + CELLS_PER_ROW);
-		let belowCell = document.getElementById(cellIdBelow);
-		y = document.getElementById(cellIdBelow).offsetTop;
-	}
-	return [x, y];
+    let y = 0;
+    if( e.offsetY <= Math.floor( CELL_WIDTH_PX / 2 ) )
+    {
+        y = parentCell.offsetTop;
+    }
+    else
+    if( parseInt(parentCell.id)+CELLS_PER_ROW > maxId )  // bottom of canvas
+    {
+        y = parentCell.offsetTop + CELL_WIDTH_PX;
+    }
+    else
+    {
+        let cellIdBelow = Pad_Start_Int(parentCellId + CELLS_PER_ROW);
+        let belowCell = document.getElementById(cellIdBelow);
+        y = document.getElementById(cellIdBelow).offsetTop;
+    }
+    return [x, y];
 }
 
 function Add_Ids_To_Palette_Cells()
 {
-	const allPaletteCells = document.querySelectorAll(".paletteCell");
-	let j = 0;
-	allPaletteCells.forEach(function(item){
-		item.id = "palette-cell-"+j;
-		j += 1;
-	})
+    const allPaletteCells = document.querySelectorAll(".paletteCell");
+    let j = 0;
+    allPaletteCells.forEach(function(item){
+        item.id = "palette-cell-"+j;
+        j += 1;
+    })
 }
 
 function Update_Active_Color_Preview()
 {
-	let activeColorDiv_1 = document.getElementById("active-color-preview-1");
-	let activeColorDiv_2 = document.getElementById("active-color-preview-2");
-	activeColorDiv_1.style.backgroundColor = STATE["firstColor"];
-	activeColorDiv_2.style.backgroundColor = STATE["secondColor"];
-	Update_Active_Color_Label();
+    let activeColorDiv_1 = document.getElementById("active-color-preview-1");
+    let activeColorDiv_2 = document.getElementById("active-color-preview-2");
+    activeColorDiv_1.style.backgroundColor = STATE["firstColor"];
+    activeColorDiv_2.style.backgroundColor = STATE["secondColor"];
+    Update_Active_Color_Label();
 }
 
 function Alert_User(text)
 {
-	let popupMessage = document.getElementById("popup-message");
-	popupMessage.classList.remove("fadeOutAnimation");
-	void popupMessage.offsetWidth;
-	popupMessage.innerHTML = text;
-	popupMessage.style.animationPlayState = "running";
-	popupMessage.classList.add("fadeOutAnimation");
+    let popupMessage = document.getElementById("popup-message");
+    popupMessage.classList.remove("fadeOutAnimation");
+    void popupMessage.offsetWidth;
+    popupMessage.innerHTML = text;
+    popupMessage.style.animationPlayState = "running";
+    popupMessage.classList.add("fadeOutAnimation");
 }
 
 function Swap_Active_Color()
 {
-	let activeColorDiv_1 = document.getElementById("active-color-preview-1");
-	let activeColorDiv_2 = document.getElementById("active-color-preview-2");
-	if(ACTIVE_COLOR_SELECT === "firstColor")
-	 {
-	   ACTIVE_COLOR_SELECT = "secondColor";
-	   activeColorDiv_2.classList.add("active_indicator");
-	   activeColorDiv_1.classList.remove("active_indicator");
-	 }
-	else
-	 {
-	   ACTIVE_COLOR_SELECT = "firstColor";
-	   activeColorDiv_1.classList.add("active_indicator");
-	   activeColorDiv_2.classList.remove("active_indicator");
-	 }
-	Update_Active_Color_Label();
+    let activeColorDiv_1 = document.getElementById("active-color-preview-1");
+    let activeColorDiv_2 = document.getElementById("active-color-preview-2");
+    if(ACTIVE_COLOR_SELECT === "firstColor")
+     {
+       ACTIVE_COLOR_SELECT = "secondColor";
+       activeColorDiv_2.classList.add("active_indicator");
+       activeColorDiv_1.classList.remove("active_indicator");
+     }
+    else
+     {
+       ACTIVE_COLOR_SELECT = "firstColor";
+       activeColorDiv_1.classList.add("active_indicator");
+       activeColorDiv_2.classList.remove("active_indicator");
+     }
+    Update_Active_Color_Label();
 }
 
 function Update_Active_Color_Label()
 {
-	activeColorLabel = document.getElementById("active-color-label");
+    activeColorLabel = document.getElementById("active-color-label");
 
-	STATE[ACTIVE_COLOR_SELECT] = Rgb_To_Hex(STATE[ACTIVE_COLOR_SELECT]);
-	activeColorLabel.innerHTML = STATE[ACTIVE_COLOR_SELECT];    // label
-	activeColorLabel.style.color = STATE[ACTIVE_COLOR_SELECT];  // text color
+    STATE[ACTIVE_COLOR_SELECT] = Rgb_To_Hex(STATE[ACTIVE_COLOR_SELECT]);
+    activeColorLabel.innerHTML = STATE[ACTIVE_COLOR_SELECT];    // label
+    activeColorLabel.style.color = STATE[ACTIVE_COLOR_SELECT];  // text color
 }
 
 function Canvas_Pixels_From_Selection(selection)
 {
-	let selectionLeft = Px_To_Int(selection.style.left);
-	let selectionTop = Px_To_Int(selection.style.top);
-	let selectionWidth = Px_To_Int(selection.style.width);
-	let selectionHeight = Px_To_Int(selection.style.height);
+    let selectionLeft = Px_To_Int(selection.style.left);
+    let selectionTop = Px_To_Int(selection.style.top);
+    let selectionWidth = Px_To_Int(selection.style.width);
+    let selectionHeight = Px_To_Int(selection.style.height);
 }
 
 function Reset_Color_Of_Canvas_Cells()
 {
-	let canvasCells = document.querySelectorAll(".canvasCell");
-	for(let i=0; i<CELLS_PER_ROW*CELLS_PER_ROW; i += 1)
-	{
-		canvasCells[i].style.backgroundColor = CANVAS_INIT_COLOR;
-	}
+    let canvasCells = document.querySelectorAll(".canvasCell");
+    for(let i=0; i<CELLS_PER_ROW*CELLS_PER_ROW; i += 1)
+    {
+        canvasCells[i].style.backgroundColor = CANVAS_INIT_COLOR;
+    }
 }
 
 function Delete_Selected()
 {
-	const selection = document.getElementById("selection");
-	if(STATE["activeTool"] === "selection")
-	{
-		if(selection != null)
-		{
-			let left = Px_To_Int(selection.style.left);
-			let top = Px_To_Int(selection.style.top);
-			let cell0 = Get_CellInt_From_CellXY(left / CELL_WIDTH_PX, top / CELL_WIDTH_PX);
-			let width = Px_To_Int(selection.style.width) / CELL_WIDTH_PX;
-			let height = Px_To_Int(selection.style.height) / CELL_WIDTH_PX;
+    const selection = document.getElementById("selection");
+    if(STATE["activeTool"] === "selection")
+    {
+        if(selection != null)
+        {
+            let left = Px_To_Int(selection.style.left);
+            let top = Px_To_Int(selection.style.top);
+            let cell0 = Get_CellInt_From_CellXY(left / CELL_WIDTH_PX, top / CELL_WIDTH_PX);
+            let width = Px_To_Int(selection.style.width) / CELL_WIDTH_PX;
+            let height = Px_To_Int(selection.style.height) / CELL_WIDTH_PX;
 
-			for(let y=0; y<height; y+=1)
-			for(let x=0; x<width; x+=1)
-			{
-				let id = Pad_Start_Int(y * CELLS_PER_ROW + cell0 + x);
-				let cell = document.getElementById(id);
-				cell.style.backgroundColor = CANVAS_INIT_COLOR;
-			}
-		}
-	}
+            for(let y=0; y<height; y+=1)
+            for(let x=0; x<width; x+=1)
+            {
+                let id = Pad_Start_Int(y * CELLS_PER_ROW + cell0 + x);
+                let cell = document.getElementById(id);
+                cell.style.backgroundColor = CANVAS_INIT_COLOR;
+            }
+        }
+    }
 }
 
 function Remove_Selection()
 {
-	let selection = document.getElementById("selection");
-	if(document.getElementById("selection"))
-		selection.remove();
+    let selection = document.getElementById("selection");
+    if(document.getElementById("selection"))
+        selection.remove();
 }
 
 function Canvas_Pixels_From_Selection()
 {
-	const selection = document.getElementById("selection");
-	let left = Px_To_Int(selection.style.left);
-	let top = Px_To_Int(selection.style.top);
-	let cell0 = Get_CellInt_From_CellXY(left / CELL_WIDTH_PX, top / CELL_WIDTH_PX);
-	let width = Px_To_Int(selection.style.width) / CELL_WIDTH_PX;
-	let height = Px_To_Int(selection.style.height) / CELL_WIDTH_PX;
+    const selection = document.getElementById("selection");
+    let left = Px_To_Int(selection.style.left);
+    let top = Px_To_Int(selection.style.top);
+    let cell0 = Get_CellInt_From_CellXY(left / CELL_WIDTH_PX, top / CELL_WIDTH_PX);
+    let width = Px_To_Int(selection.style.width) / CELL_WIDTH_PX;
+    let height = Px_To_Int(selection.style.height) / CELL_WIDTH_PX;
 
-	let colorArray = [];
-	for(let y=0; y<height; y+=1)
-	for(let x=0; x<width; x+=1)
-	{
-		let id = Pad_Start_Int(y * CELLS_PER_ROW + cell0 + x);
-		let cell = document.getElementById(id);
-		let color = Rgb_To_Hex(cell.style.backgroundColor);
-		colorArray.push(color);
-	}
-	return colorArray;
+    let colorArray = [];
+    for(let y=0; y<height; y+=1)
+    for(let x=0; x<width; x+=1)
+    {
+        let id = Pad_Start_Int(y * CELLS_PER_ROW + cell0 + x);
+        let cell = document.getElementById(id);
+        let color = Rgb_To_Hex(cell.style.backgroundColor);
+        colorArray.push(color);
+    }
+    return colorArray;
 }
 
 function CursorXY_In_Selection(cursorXY, selection)
 {
-	let selectionLeft = Px_To_Int(selection.style.left);
-	let selectionTop = Px_To_Int(selection.style.top);
-	let selectionWidth = Px_To_Int(selection.style.width);
-	let selectionHeight = Px_To_Int(selection.style.height);
+    let selectionLeft = Px_To_Int(selection.style.left);
+    let selectionTop = Px_To_Int(selection.style.top);
+    let selectionWidth = Px_To_Int(selection.style.width);
+    let selectionHeight = Px_To_Int(selection.style.height);
 
-	if( (cursorXY[0] >= selectionLeft) &&
-		(cursorXY[0] <= selectionLeft + selectionWidth) &&
-		(cursorXY[1] >= selectionTop) &&
-		(cursorXY[1] <= selectionTop + selectionHeight) )
-		return 1;
-	return 0;
+    if( (cursorXY[0] >= selectionLeft) &&
+        (cursorXY[0] <= selectionLeft + selectionWidth) &&
+        (cursorXY[1] >= selectionTop) &&
+        (cursorXY[1] <= selectionTop + selectionHeight) )
+        return 1;
+    return 0;
 }
 
 function Selection_Locked_To_Grid()
 {
-	STATE["selection"]["isLocked"] = true;
+    STATE["selection"]["isLocked"] = true;
 
-	let selection = document.getElementById("selection");
-	selection.style.outline = SELECTION_LOCKED_OUTLINE;
+    let selection = document.getElementById("selection");
+    selection.style.outline = SELECTION_LOCKED_OUTLINE;
 }
 
 function Unlock_Selection()
 {
-	STATE["selection"]["isLocked"] = false;
+    STATE["selection"]["isLocked"] = false;
 }
 
 function Toggle_Grid(e)
 {
-	const gridButton = document.getElementById("grid-button");
-	const canvasCells = document.querySelectorAll(".canvasCell");
+    const gridButton = document.getElementById("grid-button");
+    const canvasCells = document.querySelectorAll(".canvasCell");
 
-	if(STATE["grid"]["isToggled"] === false)
-	{
-		Color_Toolbar_Button_As_Down(gridButton);
-		canvasCells.forEach(function(cell) {
-			cell.style.outline = GRID_OUTLINE_CSS;
-		})
-		STATE["grid"]["isToggled"] = true;
-	}
-	else
-	{
-		Color_Toolbar_Button_When_Up(gridButton);
-		canvasCells.forEach(function(cell) {
-			cell.style.outline = "";
-		})
-		STATE["grid"]["isToggled"] = false;
-	}
+    if(STATE["grid"]["isToggled"] === false)
+    {
+        Color_Toolbar_Button_As_Down(gridButton);
+        canvasCells.forEach(function(cell) {
+            cell.style.outline = GRID_OUTLINE_CSS;
+        })
+        STATE["grid"]["isToggled"] = true;
+    }
+    else
+    {
+        Color_Toolbar_Button_When_Up(gridButton);
+        canvasCells.forEach(function(cell) {
+            cell.style.outline = "";
+        })
+        STATE["grid"]["isToggled"] = false;
+    }
 }
 
 function Transfer_Canvas_State_To_Screen(ptr)
 {
-	let savedCanvas = HISTORY_STATES.array[ptr];
-	let canvasCells = document.querySelectorAll(".canvasCell");
+    let savedCanvas = HISTORY_STATES.array[ptr];
+    let canvasCells = document.querySelectorAll(".canvasCell");
 
-	for(let i=0; i<CELLS_PER_ROW*CELLS_PER_ROW; i+=1)
-		canvasCells[i].style.backgroundColor = savedCanvas[i];
+    for(let i=0; i<CELLS_PER_ROW*CELLS_PER_ROW; i+=1)
+        canvasCells[i].style.backgroundColor = savedCanvas[i];
 }
 
 function Undo()
 {
-	HISTORY_STATES.decPtr();
-	Transfer_Canvas_State_To_Screen(HISTORY_STATES.ptr);
+    HISTORY_STATES.decPtr();
+    Transfer_Canvas_State_To_Screen(HISTORY_STATES.ptr);
 }
 
 function Redo()
 {
-	HISTORY_STATES.incPtr();
-	Transfer_Canvas_State_To_Screen(HISTORY_STATES.ptr);
+    HISTORY_STATES.incPtr();
+    Transfer_Canvas_State_To_Screen(HISTORY_STATES.ptr);
 }
 
 function Select_Palette() {
-	if(PALETTE_DISPLAY.radioNES.checked)
-	{
-		PALETTE_DISPLAY.nes.style.display = "block";
-		PALETTE_DISPLAY.gameboy.style.display = "none";
-	}
-	else
-	{
-		PALETTE_DISPLAY.nes.style.display = "none";
-		PALETTE_DISPLAY.gameboy.style.display = "block";
-	}
+    if(PALETTE_DISPLAY.radioNES.checked)
+    {
+        PALETTE_DISPLAY.nes.style.display = "block";
+        PALETTE_DISPLAY.gameboy.style.display = "none";
+    }
+    else
+    {
+        PALETTE_DISPLAY.nes.style.display = "none";
+        PALETTE_DISPLAY.gameboy.style.display = "block";
+    }
 }
 
 Color_All_Toolbar_Buttons();
--- js/utils.js
+++ js/utils.js
@@ -1,165 +1,165 @@
 function Get_Array_From_Rgb(rgb)
 {
-	rgb = rgb.replace(" ", "");
-	rgb = rgb.slice(4,rgb.length-1);
+    rgb = rgb.replace(" ", "");
+    rgb = rgb.slice(4,rgb.length-1);
 
-	let rgb_split = rgb.split(",");
+    let rgb_split = rgb.split(",");
 
-	let r = parseInt(rgb_split[0]);
-	let g = parseInt(rgb_split[1]);
-	let b = parseInt(rgb_split[2]);
+    let r = parseInt(rgb_split[0]);
+    let g = parseInt(rgb_split[1]);
+    let b = parseInt(rgb_split[2]);
 
-	return [r,g,b];
+    return [r,g,b];
 }
 
 function Rgb_To_Hex(rgb)
 {
-	if(rgb.includes("#"))
-		return rgb;
+    if(rgb.includes("#"))
+        return rgb;
 
-	rgb = rgb.replace(" ", "");
-	rgb = rgb.slice(4,rgb.length-1);
+    rgb = rgb.replace(" ", "");
+    rgb = rgb.slice(4,rgb.length-1);
 
-	let rgb_split = rgb.split(",");
+    let rgb_split = rgb.split(",");
 
-	let r = parseInt(rgb_split[0]);
-	let g = parseInt(rgb_split[1]);
-	let b = parseInt(rgb_split[2]);
+    let r = parseInt(rgb_split[0]);
+    let g = parseInt(rgb_split[1]);
+    let b = parseInt(rgb_split[2]);
 
-	return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+    return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
 }
 
 function Random_Color()
 {
-	let r = Math.floor(Math.random()*255);
-	let g = Math.floor(Math.random()*255);
-	let b = Math.floor(Math.random()*255);
-	return "rgb(" + r + ", " + g + ", " + b + ")";
+    let r = Math.floor(Math.random()*255);
+    let g = Math.floor(Math.random()*255);
+    let b = Math.floor(Math.random()*255);
+    return "rgb(" + r + ", " + g + ", " + b + ")";
 }
 
 function Get_X_From_CellInt(idx)
 {
-	return idx % CELLS_PER_ROW;
+    return idx % CELLS_PER_ROW;
 }
 
 function Get_Y_From_CellInt(idx)
 {
-	return Math.floor(idx / CELLS_PER_ROW);
+    return Math.floor(idx / CELLS_PER_ROW);
 }
 
 function Get_CellInt_From_CellXY(x, y)
 {
-	return x + (y * CELLS_PER_ROW);
+    return x + (y * CELLS_PER_ROW);
 }
 
 function Px_To_Int(str)
 {
-	// does parseInt do anything wrong?
-	return parseInt(str.slice(0, str.length-2));
+    // does parseInt do anything wrong?
+    return parseInt(str.slice(0, str.length-2));
 }
 
 function Int_To_Px(int)
 {
-	return int + "px";
+    return int + "px";
 }
 
 function Pad_Start_Int(int, pad=4)
 {
-	return int.toString().padStart(pad, 0);
+    return int.toString().padStart(pad, 0);
 }
 
 function Color_All_Toolbar_Buttons()
 {
-	let buttons = document.querySelectorAll(".toolbarButton");
-	buttons.forEach(function(b) {
-		b.style.backgroundColor = BUTTON_UP_COLOR;
-		// b.style.outline = BUTTON_UP_COLOR;
-		// BUTTON_UP_BORDER
-	})
+    let buttons = document.querySelectorAll(".toolbarButton");
+    buttons.forEach(function(b) {
+        b.style.backgroundColor = BUTTON_UP_COLOR;
+        // b.style.outline = BUTTON_UP_COLOR;
+        // BUTTON_UP_BORDER
+    })
 }
 
 function Populate_Canvas_With_Cells()
 {
-	const canvasDiv = document.getElementById("canvas-div");
-	for(let i=0; i<CELLS_PER_ROW*CELLS_PER_ROW; i+=1)
-	{
-		let div = document.createElement("div");
-		div.className = "canvasCell";
-		div.classList.add("no-select");
-		div.id = Pad_Start_Int(i);
-		div.style.backgroundColor = CANVAS_INIT_COLOR;
+    const canvasDiv = document.getElementById("canvas-div");
+    for(let i=0; i<CELLS_PER_ROW*CELLS_PER_ROW; i+=1)
+    {
+        let div = document.createElement("div");
+        div.className = "canvasCell";
+        div.classList.add("no-select");
+        div.id = Pad_Start_Int(i);
+        div.style.backgroundColor = CANVAS_INIT_COLOR;
 
-		canvasDiv.appendChild(div);
-	}
+        canvasDiv.appendChild(div);
+    }
 }
 
 function Populate_Palette_With_Cells()
 {
-	const paletteDiv = document.getElementById("palette-div");
+    const paletteDiv = document.getElementById("palette-div");
 
-	for(let i=0; i<palette_color_array.length; i += 1)
-	{
-		let cell = document.createElement("div");
-		cell.className = "paletteCell";
-		cell.style.backgroundColor = palette_color_array[i];
-		paletteDiv.appendChild(cell);
-	}
+    for(let i=0; i<palette_color_array.length; i += 1)
+    {
+        let cell = document.createElement("div");
+        cell.className = "paletteCell";
+        cell.style.backgroundColor = palette_color_array[i];
+        paletteDiv.appendChild(cell);
+    }
 }
 
 function Populate_GBPalette_With_Cells()
 {
-	const gameboyPaletteDiv = document.getElementById("gameboy-palette-div");
+    const gameboyPaletteDiv = document.getElementById("gameboy-palette-div");
 
-	for(let i=0; i<gb_palette_color_array.length; i += 1)
-	{
-		let cell = document.createElement("div");
-		cell.className = "paletteCell";
-		cell.style.backgroundColor = gb_palette_color_array[i];
-		gameboyPaletteDiv.appendChild(cell);
-	}
+    for(let i=0; i<gb_palette_color_array.length; i += 1)
+    {
+        let cell = document.createElement("div");
+        cell.className = "paletteCell";
+        cell.style.backgroundColor = gb_palette_color_array[i];
+        gameboyPaletteDiv.appendChild(cell);
+    }
 }
 
 function Update_Tooltip_Text()
 {
-	for(label in Tools)
-	{
-		let id = Tools[label]["button-id"];
-		let hotkey = Tools[label]["hotkey"];
+    for(label in Tools)
+    {
+        let id = Tools[label]["button-id"];
+        let hotkey = Tools[label]["hotkey"];
 
-		const btn = document.getElementById(id);
-		const tooltip = btn.children[0];
-		tooltip.innerHTML = label + "<span class='hotkeyText'>" + " (" + hotkey[3] + ")" + "</span>";
-	}
+        const btn = document.getElementById(id);
+        const tooltip = btn.children[0];
+        tooltip.innerHTML = label + "<span class='hotkeyText'>" + " (" + hotkey[3] + ")" + "</span>";
+    }
 }
 
 function Set_Cursor(newCursorString)
 {
-	document.getElementById("canvas-div").style.cursor = newCursorString;
+    document.getElementById("canvas-div").style.cursor = newCursorString;
 }
 
 function Get_Cursor()
 {
-	return document.getElementById("canvas-div").style.cursor;
+    return document.getElementById("canvas-div").style.cursor;
 }
 
 function Color_Toolbar_Button_As_Down(elem)
 {
-	elem.style.backgroundColor = BUTTON_DOWN_COLOR;
-	elem.style.outline = BUTTON_DOWN_OUTLINE;
+    elem.style.backgroundColor = BUTTON_DOWN_COLOR;
+    elem.style.outline = BUTTON_DOWN_OUTLINE;
 }
 
 function Color_Toolbar_Button_When_Up(elem)
 {
-	elem.style.backgroundColor = BUTTON_UP_COLOR;
-	elem.style.outline = BUTTON_UP_OUTLINE;
+    elem.style.backgroundColor = BUTTON_UP_COLOR;
+    elem.style.outline = BUTTON_UP_OUTLINE;
 }
 
 function Get_Canvas_Pixels()
 {
-	let canvasCells = document.querySelectorAll(".canvasCell");
-	let canvasPixels = [];
-	canvasCells.forEach(function(cell){
-		canvasPixels.push(cell.style.backgroundColor);
-	})
-	return canvasPixels;
+    let canvasCells = document.querySelectorAll(".canvasCell");
+    let canvasPixels = [];
+    canvasCells.forEach(function(cell){
+        canvasPixels.push(cell.style.backgroundColor);
+    })
+    return canvasPixels;
 }
